5abb0a349002789deede5a6714b921568278b7e1
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2022-12-27 16:39:24 +0000

    update packages add post request

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import * as pg from 'pg'
 import cors from 'cors';
+import bodyParser from 'body-parser';
 const { Pool } = pg.default
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const pool = new Pool({
-  user: 'neilg',
+  user: 'Tay.Duncan',
   host: 'localhost',
   database: 'demo',
   password: 'password',
@@ -21,6 +23,17 @@ app.get('/', (req, res) => {
 
 app.get('/stock', (req, res) => {
   pool.query('SELECT * FROM demo.stock ORDER BY stock_id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+});
+
+app.post('/add', (req, res) => {
+  let { name, description } = req.body;
+console.log({ name, description })
+  pool.query(`INSERT INTO demo.stock (name, description) VALUES ('${name}', '${description}');`, (error, results) => {
     if (error) {
       throw error
     }
